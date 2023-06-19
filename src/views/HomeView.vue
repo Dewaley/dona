d6d523be3147c2bb1585bbda6318a253412faa9b
Dewaley <adewaleademilua@gmail.com>
2023-06-19 02:27:02 +0100

    responsive design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,15 @@ import Sidebar from "../components/Sidebar.vue";
   <!-- <h2>HomeView &#127968;</h2> -->
   <div class="main">
     <Sidebar />
+    <main>
+      <div class="header">
+        <h2>Good night Dewaley,</h2>
+        <h2><span>It's Tuesday, Jun 20 - 7 tasks</span></h2>
+      </div>
+      <form>
+        <input type="text" name="" id="" />
+      </form>
+    </main>
   </div>
 </template>
 
@@ -13,5 +22,6 @@ import Sidebar from "../components/Sidebar.vue";
 .main {
   background-color: #eceeef;
   min-height: 100vh;
+  display: flex;
 }
 </style>
